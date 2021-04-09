bfb85c7e4856d0d35efbd7b2e9c7d89c8d423dbf
Anotnio <lateralprogramming@gmail.com>
2021-04-09 15:31:30 -0700

    saving

--- app/models.py
+++ app/models.py
@@ -1,22 +1,21 @@
-import os
 from app import db
-from datetime import DateTime
+from datetime import datetime
 
 
-class Divvy(self):
+class Divvy(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    trip_id = db.Column(db.Integer)
-    starttime = db.Column(db.DateTime())
-    stoptime = db.Column(db.DateTime())
-    bikeid = db.Column(db.Integer)
-    from_station_id = db.Column(db.Integer)
-    from_station_name = db.Column(db.String(50))
-    to_station_id = db.Column(db.Integer)
-    to_station_name = db.Column(db.String(50))
-    usertype = db.Column(db.String(20))
-    gender = db.Column(db.String(10))
-    birthday = db.Column(db.DateTime)
-    trip_duration = db.Column(db.Integer)
+    trip_id = db.Column(db.Integer, nullable=True)
+    starttime = db.Column(db.DateTime(), nullable=True)
+    stoptime = db.Column(db.DateTime(), nullable=True)
+    bikeid = db.Column(db.Integer, nullable=True)
+    from_station_id = db.Column(db.Integer, nullable=True)
+    from_station_name = db.Column(db.String(50), nullable=True)
+    to_station_id = db.Column(db.Integer, nullable=True)
+    to_station_name = db.Column(db.String(50), nullable=True)
+    usertype = db.Column(db.String(20), nullable=True)
+    gender = db.Column(db.String(10), nullable=True)
+    birthday = db.Column(db.String(10), nullable=True)
+    trip_duration = db.Column(db.Integer, nullable=True)
 
     def __init__(self, trip_id, starttime, stoptime, bikeid, from_station_id, from_station_name, to_station_id, to_station_name, usertype, gender, birthday, trip_duration):
         self.trip_id = trip_id
--- run.py
+++ run.py
@@ -1,12 +1,11 @@
-from app import create_app, db
+from app import app, db
 from app.models import Divvy
 
 if __name__ == '__main__'".env":
-    App.run(host=localhost, port=5555)
+    app.run(host=localhost, port=5555)
 
 
-app = create_app()
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'Post': Post, 'Kekambas': Kekambas, 'Product': Product}
+    return {'db': db, 'Divvy': Divvy}
